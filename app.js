be58efff6e86d86cd2f741d20d71a0cbd4c7d9c5
Nika <nabakhtevelin@gmail.com>
2021-08-04 02:22:02 +0400

    added bugs to fix later

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ import fs from 'fs';
 const app = express();
 const fileStorageEngine = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './assets/serverImages')
+    cb(null, './assets/tvshow-serverImages')
   },
     filename: (req, file, cb) => {
-      cb(null, `${file.originalname}`);
+      cb(null, file.originalname);
     }
   }
 );
@@ -37,26 +37,154 @@ app.get('/for-TV-show', (req, res) => {
   res.render(__dirname + "/snippet/tvshow.pug");
 });
 
+app.get("/for-TV-show/TVshow-success", [urlencodedParser, upload.single("image")], (req, res) => {
+  console.log(req.query);
+  res.render(__dirname + "/snippet/TVshow-success", {
+    guestName: req.query.guestName, 
+    guestImage: req.query.image,
+    secondGuest: req.query.secondGuestName,
+    secondUserImage: req.query.secondImage
+  });
 
-app.post("/post", [urlencodedParser, upload.single("image")], (req, res) => {
+  const pathForImage = '/assets/tvshow-serverImages/template.png'
+
+  async function download(url = defaultURL, path = pathForImage, callback) {
+    if(url === "") {
+      url = defaultURL;
+    }
+    request.head(url, (err, res, body) => {
+      request(url)
+        .pipe(fs.createWriteStream(path))
+        .on('close', callback)
+    })
+  }
+
+
+  nodeHtmlToImage({
+    output: './assets/tvshow-serverImages/template.png',
+    html: `
+    <html>
+    <style>
+      @import url("//cdn.web-fonts.ge/fonts/bpg-nino-mtavruli/css/bpg-nino-mtavruli.min.css");
+      @import url("//cdn.web-fonts.ge/fonts/bpg-web-002/css/bpg-web-002.min.css");
+      @import url("./assets/post/css/tvshow-success.css");
+
+      body {
+        overflow: hidden;
+        width: 2048px;
+        height: 2048px;
+      }
+
+      .post-container {
+        width: 2048px;
+        height: 2048px;
+        transform-origin: top left;
+        margin: 50px 0;
+        left: 50%;
+        transform: translateX(-50%);
+        font-family: "BPG WEB 002", sans-serif;
+        text-align: center;
+        position: relative;
+        color: white;
+        overflow: hidden;
+      }
+
+      .post-container::after {
+        content: "";
+        position: absolute;
+        left: 0;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        pointer-events: none;
+      }
+
+      .content {
+        margin-top: -45px;
+      }
+
+      .img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+
+      .text {
+        width: 95%;
+        height: 90%;
+        position: absolute;
+        left: 0;
+        top: 40px;
+        display: flex;
+        font-family: "BPG WEB 002", sans-serif;
+        justify-content: center;
+        align-items: flex-end;
+        z-index: 100;
+
+      }
+
+      .text p {
+        width: 1555px;
+        font-weight: bold;
+        font-size: 70px;
+        font-family: "BPG WEB 002", sans-serif;
+        line-height: 100px;
+      }
+
+      .author {
+        position: absolute;
+        bottom: 35px;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        font-size: 76px;
+        line-height: 87px;
+        z-index: 100;
+
+      }
+
+      .author::after {
+        content: "";
+        position: absolute;
+        bottom: -22px;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        height: 10px;
+        width: 100%;
+        background-color: #08a53f;
+        border-radius: 21px;
+      }
+
+      #btn {
+        margin: 20px 0 0 20px;
+      }
+
+    </style>
+      <body>
+        <div class="post-container">
+          <img src={{picture}} class="img">
+          <div class="text">
+              <p>{{author}}</p>
+          </div>
+          <div class="author">{{message}}</div>
+        </div>
+      </body>
+    </html>`,
+
+    content: {
+      author: req.body.message,
+      message: req.body.name,
+      // picture: `http://127.0.0.1:3000/assets/serverImages/${req.file.originalname}`
+    }
+  }).then(() => console.log('The image was created successfully!'))
+});
+
+
+app.get("/post", [urlencodedParser, upload.single("image")], (req, res) => {
   res.render(__dirname + "/snippet/post-success", {
     data: req.body, 
     image: `./assets/serverImages/${req.file.originalname}`,
     generatedImage: " image.png"
   });
-
-  // Writing template's data to the JSON file for an API
-  let data = JSON.stringify({
-    author: req.body.name,
-    message: req.body.message,
-    img_path: `./assets/serverImages/${req.file.originalname}`,
-  });
-
-  fs.writeFile("templateProps.json", data, (err) => {
-    if (err) throw err;
-    console.log('Data has been written to the file');
-  });
-
 
   nodeHtmlToImage({
     output: './assets/post/generatedTemplate/image.png',
--- assets/js/twshow.js
+++ assets/js/twshow.js
@@ -15,24 +15,24 @@ const appendSecondForm = (function() {
         
                 if(isFocused) {
                    const HTMLtoAppend = `
-                   <div class="rows-container">
+                    <div class="rows-container">
                         <div class="col-md-6 form-group mb-3">
                             <label class="col-form-label">სტუმარი</label>
-                            <input class="form-control" id="name" for="guest" type="text" name="guest-name" placeholder="სტუმარის სახელი"/>
+                            <input class="form-control" id="name" for="guest" type="text" name="guestName" placeholder="სტუმარის სახელი"/>
                         
-                        <div class="form-group">
-                            <label for="exampleFormControlFile1">სურათის ატვირთვა</label>
-                            <input class="form-control-file" id="exampleFormControlFile1" for="image" name="image" type="file" required='true'/>
-                        </div>
-                    </div>
-                    <br>
-                        <div class="col-md-6 form-group mb-3">
-                            <label class="col-form-label">მეორე სტუმარი</label>
-                            <input class="form-control" id="name" for="guest" type="text" name="guest-name" placeholder="სტუმარის სახელი"/>
-                            
                             <div class="form-group">
                                 <label for="exampleFormControlFile1">სურათის ატვირთვა</label>
                                 <input class="form-control-file" id="exampleFormControlFile1" for="image" name="image" type="file" required='true'/>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="col-md-6 form-group mb-3">
+                            <label class="col-form-label">მეორე სტუმარი</label>
+                            <input class="form-control" id="name" for="secondGuest" type="text" name="secondGuestName" placeholder="სტუმარის სახელი"/>
+                            
+                            <div class="form-group">
+                                <label for="exampleFormControlFile1">სურათის ატვირთვა</label>
+                                <input class="form-control-file" id="exampleFormControlFile1" for="secondImage" name="secondImage" type="file" required='true'/>
                             </div>
                         </div>
                     </div>
@@ -53,7 +53,7 @@ const appendSecondForm = (function() {
                         <div class="row"></div>
                         <div class="col-md-6 form-group mb-3">
                             <label class="col-form-label">სტუმარი</label>
-                            <input class="form-control" id="name" for="guest" type="text" name="guest-name" placeholder="სტუმარის სახელი" required=''/>
+                            <input class="form-control" id="name" for="guest" type="text" name="guestName" placeholder="სტუმარის სახელი" required=''/>
                         </div>
                         <div class="form-group">
                             <label for="exampleFormControlFile1">სურათის ატვირთვა</label>
